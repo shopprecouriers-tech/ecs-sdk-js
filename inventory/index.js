ff84ffb67a3370026d283c75b8532b0fc120c795
punihs <punith@shoppre.com>
2020-05-07 12:00:45 -0200

    feat(): Shipment creation

--- inventory/index.js
+++ inventory/index.js
@@ -5,7 +5,7 @@ const { OMNIINVENTORY_URL, OMNIINVENTORY_SECRET } = process.env;
 const log = debug('omniinventory/index.js');
 
 exports.createSale = (payload) => {
-    log('createTransaction()', payload);
+    log('createSale()', payload);
     if(!payload) {
         return Promise.reject({
             code: 400,
@@ -15,6 +15,22 @@ exports.createSale = (payload) => {
     return rp({
         method: 'POST',
         uri: `${OMNIINVENTORY_URL}/api/sales?token=${OMNIINVENTORY_SECRET}`,
+        body: payload,
+        json: true,
+    })
+};
+
+exports.createShipment = (payload) => {
+    log('createshipment()', payload);
+    if(!payload) {
+        return Promise.reject({
+            code: 400,
+            message: 'Pass payload{}  argument to createShipment method',
+        })
+    }
+    return rp({
+        method: 'POST',
+        uri: `${OMNIINVENTORY_URL}/api/shipments?token=${OMNIINVENTORY_SECRET}`,
         body: payload,
         json: true,
     })
