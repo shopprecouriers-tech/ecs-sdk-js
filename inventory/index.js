f810c91aec43495cf056fc68ba8c091fc32d1bfb
punihs <punith@shoppre.com>
2020-05-10 14:30:05 -0200

    fix(): created file required to publish

--- inventory/index.js
+++ inventory/index.js
@@ -1,5 +1,5 @@
-const sale = require('./api/sale');
-const shipment = require('./api/shipment');
+const sale = require('./api/sale/sale.service');
+const shipment = require('./api/shipment/shipment.service');
 
 module.exports = {
   sale,
