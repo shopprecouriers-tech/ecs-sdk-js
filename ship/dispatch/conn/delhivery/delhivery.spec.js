43b011127194814bae1a231208f3b023c1ffb3ea
vickey localhost.localdomain <vickey@localhost.localdomain>
2020-05-11 13:11:38 +0530

    feat(Delhivery): Delivery order object data updation

--- ship/dispatch/conn/delhivery/delhivery.spec.js
+++ ship/dispatch/conn/delhivery/delhivery.spec.js
@@ -5,7 +5,47 @@ const deliveryService = require('./');
 describe('Delhivery Order', () => {
   before(() => {
     sinon.stub(rp, 'Request')
-      .resolves({ packages: [{ waybill: '1655610631794' }] });
+      .resolves({
+        cash_pickups_count: 0,
+        package_count: 2,
+        upload_wbn: 'UPL14204591278668202717',
+        replacement_count: 0,
+        rmk: 'An internal Error has occurred, Please get in touch with client.support@delhivery.com',
+        pickups_count: 0,
+        packages: [
+          {
+            status: 'Fail',
+            client: 'CURESKIN EXPRESS',
+            sort_code: null,
+            remarks: [
+              'Duplicate order id',
+            ],
+            waybill: '1655610631794',
+            cod_amount: 1000,
+            payment: 'COD',
+            serviceable: false,
+            refnum: 'stest0021',
+          },
+          {
+            status: 'Fail',
+            client: 'CURESKIN EXPRESS',
+            sort_code: null,
+            remarks: [
+              'Duplicate order id',
+            ],
+            waybill: '1655610631805',
+            cod_amount: 1000,
+            payment: 'COD',
+            serviceable: false,
+            refnum: 'stest0022',
+          },
+        ],
+        cash_pickups: 0,
+        cod_count: 0,
+        success: false,
+        prepaid_count: 0,
+        cod_amount: 0,
+      });
   });
 
   it('should create sale', async () => {
