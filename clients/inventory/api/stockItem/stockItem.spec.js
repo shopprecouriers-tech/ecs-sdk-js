5f03e7989c301974ffaf40bbf5fdeadec0feccf4
punihs <punith@shoppre.com>
2020-06-10 23:12:50 -0200

    feat(): stock item update api

--- clients/inventory/api/stockItem/stockItem.spec.js
+++ clients/inventory/api/stockItem/stockItem.spec.js
@@ -5,12 +5,30 @@ const stockItemService = require('./stockItem.service');
 describe('Inventory stock Item', () => {
   it('should create Stock Item', async () => {
     const stockItemId = await stockItemService.create({
-      description: 'Fash Foam face wash',
-      unit: 'TAB.',
-      cost_amount: 450,
-      gst_percentage: 18,
+      description: 'expensive Shampoo',
+      hsn_code: '897846',
+      quantity: 1,
+      cost_amount: 320,
+      discount: 100,
+      total: 220,
+      gst_percentage: 12,
     });
     expect(stockItemId.id);
   });
 });
 
+describe('Inventory stock Item', () => {
+  it('should Update Stock Item', async () => {
+    const stockItemId = await stockItemService.update({
+      description: 'Expensive Soap',
+      hsn_code: '897846',
+      quantity: 2,
+      cost_amount: 320,
+      discount: 100,
+      total: 220,
+      gst_percentage: 12,
+      id: 229,
+    });
+    expect(stockItemId.id);
+  });
+});
